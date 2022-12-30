0a2e8524e4e29c163e6a4f8134b397c3c0783699
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-12-30 20:28:27 +0200

    2

---
+++ src/components/ImageGallery/ImageGalleryPendingView/ImageGalleryPendingView.jsx
@@ -0,0 +1,5 @@
+import { BallTriangle } from  'react-loader-spinner'
+
+export const ImageGalleryPendingView = () => {
+    return <BallTriangle wrapperStyle={{justifyContent: 'center', alignItems: 'center'}} />
+}
